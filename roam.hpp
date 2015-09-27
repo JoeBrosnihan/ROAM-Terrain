6313f112a615a4936df189315d6896f8ea2425ef
JoeBrosnihan <joebrosnihan@gmail.com>
2015-09-26 23:15:57 -0400

    Create ROAMImpl that will manage the ROAM algorithm
    ROAMImpl contains a priority queue of triangles. Add priority to
    each triangle for comparison in the priority queue.

---
+++ roam.hpp
@@ -0,0 +1,12 @@
+#ifndef ROAM_HPP
+#define ROAM_HPP
+
+#include <queue>
+
+class ROAMImpl {
+	public:
+	private:
+		std::priority_queue<struct tri> split_queue;
+};
+
+#endif
--- triangle.hpp
+++ triangle.hpp
@@ -9,6 +9,16 @@ struct tri {
 	vec3 va, v0, v1;
 	//Left and right subtriangles
 	struct tri *tri0, *tri1;
+	//Priority metric (ammount of error)
+	float priority;
+
+	//Triangle comparison for priority_gueues
+	inline bool operator<(struct tri other_tri) {
+		if (priority < other_tri.priority)
+			return true;
+		else
+			return false;
+	}
 }
 
 #endif
